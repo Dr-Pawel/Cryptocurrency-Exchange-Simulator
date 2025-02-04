6c431e4a22f0127f6b426110ca9a62bb9f59c30b
DrPawel <Paulnatoryt@gmail.com>
2025-02-04 19:33:50 +0100

    Initial Commit

---
+++ Cryptocurrency_Exchange_Simulator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Cryptocurrency_Exchange_Simulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
