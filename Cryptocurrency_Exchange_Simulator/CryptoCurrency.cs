587fffb1ec554779c3c3df07b582ff3ec5167f57
DrPavel <Paulnatoryt@gmail.com>
2025-02-04 22:25:34 +0100

    Created Class - CryptoCurrency and Portfolio to controle user ballance and holdings

---
+++ Cryptocurrency_Exchange_Simulator/CryptoCurrency.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptocurrency_Exchange_Simulator
+{
+    internal class CryptoCurrency
+    {
+        public string Name { get; set; }
+        public string Symbol { get; set; }
+        public decimal Price { get; set; }
+
+        public CryptoCurrency(string name, string symbol, decimal price)
+        {
+            Name = name;
+            Symbol = symbol;
+            Price = price;
+        }
+
+        public void updatePrice(Random rng)
+        {
+            decimal changePercentage = (decimal)rng.Next(-10, 11) / 100;
+            Price += Price * changePercentage;
+            Price = Math.Round(Price, 2);
+        }        
+    }
+}
---
+++ Cryptocurrency_Exchange_Simulator/Portfolio.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptocurrency_Exchange_Simulator
+{
+    internal class Portfolio
+    {
+        public decimal Balance { get; set; }
+        Dictionary<string, decimal> Holdings = new Dictionary<string, decimal>();
+
+        public Portfolio(decimal balance)
+        {
+            balance = 10000;
+            Balance = balance;
+            Console.WriteLine($"Current balance: {Balance}$");
+        }
+
+        public void BuyCrypto(string symbol, decimal amount, CryptoCurrency crypto)
+        {
+
+        }
+
+        public void SellCrypto(string symbol, decimal amount, CryptoCurrency crypto)
+        {
+
+        }
+        public void ShowPortfolio()
+        {
+            Console.WriteLine($"Current Ballance: {Balance}$");
+
+            if(Holdings.Count == 0)
+            {
+                Console.WriteLine("You currently dont have any cryptocurrencies");
+            }
+            else
+            {
+                foreach (var asset in Holdings)
+                {
+                    Console.WriteLine($"{asset.Key}: {asset.Value} units");
+                }
+            }
+        }
+    }
+}
